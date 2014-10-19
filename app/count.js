d9d3683d0de59b8252090f61adc3d4663170f9fe
Sarah <sarah.ransohoff@venmo.com>
2014-10-19 18:08:23 -0400

    Start passing tests for counter, not entirely sure what they're looking for.

--- app/count.js
+++ app/count.js
@@ -3,6 +3,17 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var shouldBreak = false;
+      var result = {
+        cancel: function() { shouldBreak = true; }
+      };
+      var current = start;
+      while ( current <= end ) {
+        if ( shouldBreak ) { break; }
+        console.log(current);
+        current ++;
+      }
+      return result;
     }
   };
 });
