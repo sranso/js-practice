5fa40247c7748fc577161f476f620d7a9e93063b
Sarah <sarah.ransohoff@venmo.com>
2014-10-20 00:11:15 -0400

    Pass objects tests.

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      this.greeting = obj.greeting;
+      this.name = obj.name;
+      return fn.call(this);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
+      return constructor;
     },
 
     iterate : function(obj) {
-
+      var keys = Object.getOwnPropertyNames(obj);
+      var ownProps = [];
+      keys.forEach(function(key) {
+        ownProps.push(key + ': ' + obj[key]);
+      });
+      return ownProps;
     }
   };
 });
