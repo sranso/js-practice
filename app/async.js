fa7459b2d3bf76a161f69a418093303ea64927ff
Sarah <sarah.ransohoff@venmo.com>
2014-10-27 13:04:02 -0400

    Pass async.

--- app/async.js
+++ app/async.js
@@ -3,7 +3,13 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+      return new Promise(function(resolve, reject) {
+        if ( value ) {
+          resolve(value);
+        } else {
+          reject(Error('fail!'));
+        }
+      });
     },
 
     manipulateRemoteData : function(url) {
