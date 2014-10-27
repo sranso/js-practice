393bf44fb2f9776daf65e90b912c38723351405c
Sarah <sarah.ransohoff@venmo.com>
2014-10-27 13:01:03 -0400

    Pass manipulateRemoteData.

--- app/async.js
+++ app/async.js
@@ -7,7 +7,27 @@ define([ 'jquery' ], function($) {
     },
 
     manipulateRemoteData : function(url) {
+      return new Promise(function(resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open('GET', url);
 
+        req.onload = function() {
+          if ( req.status === 200 ) {
+            var response = JSON.parse(req.response).people.map(function(item) {
+              return item.name;
+            }).sort();
+            resolve(response);
+          } else {
+            reject(Error(req.statusText));
+          }
+        };
+
+        req.onerror = function() {
+          reject(Error('Network Error'));
+        };
+
+        req.send();
+      });
     }
   };
 });
