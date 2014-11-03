5a05857c54bc2c3de4b483a0d63646a767d31fb0
Sarah <sarah.ransohoff@venmo.com>
2014-11-03 10:31:31 -0500

    Pass first recursion test.

--- app/recursion.js
+++ app/recursion.js
@@ -1,9 +1,18 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-
 define(function() {
   return {
     listFiles: function(data, dirName) {
+      var fileNames = dirName || [];
 
+      for ( var i = 0; i < data.files.length; i++ ) {
+        if ( typeof data.files[i] === 'string' ) {
+          fileNames.push(data.files[i]);
+        } else if ( typeof data.files[i] === 'object' ) {
+          this.listFiles(data.files[i], fileNames);
+        }
+      }
+
+      return fileNames;
     },
 
     permute: function(arr) {
